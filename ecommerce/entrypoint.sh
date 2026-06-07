c3da4f1fd1e7d22b9cc4edf8b741f5ed0140dcfb
sofiakharbouche02-crypto <sofiakharbouche02@gmail.com>
2026-06-08 00:44:31 +0100

    Create entrypoint.sh

---
+++ ecommerce/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Demarrage de l'application Django..."
+
+echo "Application des migrations..."
+python manage.py migrate --noinput
+
+echo "Collecte des fichiers statiques..."
+python manage.py collectstatic --noinput
+
+echo "Lancement de l'application..."
+exec "$@"
